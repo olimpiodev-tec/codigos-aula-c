25c5cc195d6775f4b1634a033bb42df1482f6ead
Moisés <olimpiodev.tec@gmail.com>
2026-05-13 16:21:32 -0300

    Exercicio localiza

--- exercicios/localiza.c
+++ exercicios/localiza.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <locale.h>
+#include <string.h>
 
 int main()
 {
@@ -8,28 +9,64 @@ int main()
     int km = 0;
     int dias = 0;
     char nome[30];
+    char carro[30];
     float totalPorKm = 0;
     float totalPorDia = 0;
     const float VALOR_POR_KM = 1.25;
     const float VALOR_POR_DIA = 99.50;
+    const char cupons[3][30] = {
+        "MFOF_10", "MFOF_20", "MFOF_30"
+    };
+    char cupomCliente[30];
+    int temCupom = 0;
+    float valorDesconto = 0;
 
     printf("Olá seja bem vindo, qual seu nome?");
     scanf("%s", nome);
 
     printf("Obrigado por escolher nossa loja, %s", nome);
 
-    printf("\nQuantos KMs você vai rodar com o carro?");
+    printf("\n%s, qual carro deseja alugar?", nome);
+    scanf("%s", carro);
+
+    printf("\nQuantos KMs com o %s você vai rodar com o carro?", carro);
     scanf("%i", &km);
 
     printf("Para mostrar um relatório detalhado diga quantos dias vai precisar do carro?");
     scanf("%i", &dias);
 
-    totalPorKm = km * VALOR_POR_KM;
-    totalPorDia = dias * VALOR_POR_DIA;
+    printf("%s, tem cupom de desconto? 1->sim, 0->não", nome);
+    scanf("%i", &temCupom);
+
+    if (temCupom == 1) {
+        printf("Digite o código: ");
+        scanf("%s", cupomCliente);
+
+        for (int i=0; i < 3; i++) {
+            if (strcmp(cupomCliente, "MFOF_10") == 0) {
+                valorDesconto = 10;
+            }
+            if (strcmp(cupomCliente, "MFOF_20") == 0) {
+                valorDesconto = 20;
+            }
+            if (strcmp(cupomCliente, "MFOF_30") == 0) {
+                valorDesconto = 30;
+            }
+        }
+
+    }
+
+
+    totalPorKm = (km * VALOR_POR_KM) - valorDesconto;
+    totalPorDia = (dias * VALOR_POR_DIA) - valorDesconto;
 
     printf("%s, segue um relatório detalhado", nome);
     printf("\nTotal por KM: R$ %.2f", totalPorKm);
     printf("\nTotal por Dia: R$ %.2f", totalPorDia);
+
+    if (temCupom == 1) {
+        printf("\nVocê teve desconto de R$ %.2f", valorDesconto);
+    }
 
     if (totalPorDia < totalPorKm) {
         printf("\nSugerimos usar o plano por dia");
@@ -38,6 +75,7 @@ int main()
     }
 
     printf("\nTenha uma ótima tarde.");
+    printf("\n Desenvolvido por Moises F.O.F");
 
     return 0;
 }
